e108a1f8e0857c0cb19dafb92cfd66080492c520
dava-alt-f4 <davagalangsamudra06@gmail.com>
2026-09-01 11:24:46 +0700

    small formatting

--- resources/js/pages/admin/Dashboard.vue
+++ resources/js/pages/admin/Dashboard.vue
@@ -12,12 +12,12 @@ import {
     DialogFooter,
     DialogTrigger,
     DialogDescription,
+    DialogClose,
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Spinner } from '@/components/ui/spinner';
 import { dashboard } from '@/routes/admin';
-import DialogClose from '@/components/ui/dialog/DialogClose.vue';
 
 defineProps<{
     users: Array<{
