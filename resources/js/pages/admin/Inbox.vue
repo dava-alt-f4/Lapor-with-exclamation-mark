4dce810edac2350b9110378a0b14e77ffd07f546
dava-alt-f4 <davagalangsamudra06@gmail.com>
2026-09-03 13:37:40 +0700

    small fix inbox form not reset

--- resources/js/pages/admin/Inbox.vue
+++ resources/js/pages/admin/Inbox.vue
@@ -121,7 +121,7 @@ useEcho<InboxMessage>('admin.inbox', '.MessageSent', (message) => {
         scrollToBottom();
     }
 
-    if (props.activeConversation?.id === message.conversation_id) {
+    if (props.activeConversation?.id === message.conversation_id && message.sender.role !== 'admin') {
         markConversationAsRead(message.conversation_id);
     }
 });
